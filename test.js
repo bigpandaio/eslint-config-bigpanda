497d28023a68f383dd3cb762755c1b1c6784b6eb
Gilad Foyer <gilad.foyer@gmail.com>
2016-05-06 17:15:20 +0300

    add modules.export

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-"extends": "./index.js",
-"globals": {
-  "expect": true,
-  "sinon": true,
-  "proxyquire": true
-},
-"rules": {
-  "no-unused-expressions": "off"
+module.exports = {
+  "extends": "./index.js",
+  "globals": {
+    "expect": true,
+    "sinon": true,
+    "proxyquire": true
+  },
+  "rules": {
+    "no-unused-expressions": "off"
+  }
 }
