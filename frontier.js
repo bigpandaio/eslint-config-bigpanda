d57b27028b81650eb7fe5092aaa1cb583bbb3f91
Idan Cohen <idan@idancohen.com>
2017-03-19 14:58:02 +0200

    change dependencies

---
+++ frontier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "./index.js",
+  "globals": {
+    "expect": true,
+    "sinon": true,
+    "proxyquire": true
+  },
+  "rules": {
+    "no-unused-expressions": "off",
+    "arrow-body-style": "off"
+  }
+}
