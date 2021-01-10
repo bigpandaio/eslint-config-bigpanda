19701aede8c6949f0c8c0e3a1826feface143ab2
Ori Riner <ori.riner@bigpanda.io>
2021-01-10 14:12:39 +0200

    Enable eslint-jsx-a11y-plugin

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 module.exports = {
-    "extends": "airbnb",
+    "extends": [
+      "airbnb",
+      "plugin:jsx-a11y/recommended"
+    ],
     "env": {
       "es6": true,
       "node": true,
       "mocha": true
     },
     "plugins": [
-      "no-only-tests"
+      "no-only-tests",
+      "jsx-a11y"
     ],
     "rules": {
       // Overrides
@@ -22,40 +26,6 @@ module.exports = {
       "implicit-arrow-linebreak": 0,
       "import/no-extraneous-dependencies": 0,
       "import/no-unresolved": 0,
-      "jsx-a11y/anchor-has-content": 0,
-      "jsx-a11y/aria-role": 0,
-      "jsx-a11y/aria-props": 0,
-      "jsx-a11y/aria-proptypes": 0,
-      "jsx-a11y/aria-unsupported-elements": 0,
-      "jsx-a11y/alt-text": 0,
-      "jsx-a11y/img-redundant-alt": 0,
-      "jsx-a11y/label-has-for": 0,
-      "jsx-a11y/label-has-associated-control": 0,
-      "jsx-a11y/mouse-events-have-key-events": 0,
-      "jsx-a11y/no-access-key": 0,
-      "jsx-a11y/no-onchange": 0,
-      "jsx-a11y/interactive-supports-focus": 0,
-      "jsx-a11y/role-has-required-aria-props": 0,
-      "jsx-a11y/role-supports-aria-props": 0,
-      "jsx-a11y/tabindex-no-positive": 0,
-      "jsx-a11y/heading-has-content": 0,
-      "jsx-a11y/html-has-lang": 0,
-      "jsx-a11y/lang": 0,
-      "jsx-a11y/no-distracting-elements": 0,
-      "jsx-a11y/scope": 0,
-      "jsx-a11y/click-events-have-key-events": 0,
-      "jsx-a11y/no-static-element-interactions": 0,
-      "jsx-a11y/no-noninteractive-element-interactions": 0,
-      "jsx-a11y/accessible-emoji": 0,
-      "jsx-a11y/aria-activedescendant-has-tabindex": 0,
-      "jsx-a11y/iframe-has-title": 0,
-      "jsx-a11y/no-autofocus": 0,
-      "jsx-a11y/no-redundant-roles": 0,
-      "jsx-a11y/media-has-caption": 0,
-      "jsx-a11y/no-interactive-element-to-noninteractive-role": 0,
-      "jsx-a11y/no-noninteractive-element-to-interactive-role": 0,
-      "jsx-a11y/no-noninteractive-tabindex": 0,
-      "jsx-a11y/anchor-is-valid": 0,
       "react/jsx-one-expression-per-line": 0,
 
       // Warnings
